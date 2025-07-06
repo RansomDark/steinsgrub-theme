a8165ac48a67dbac2da861eb506d48397b1e2e63
RansomDark <ransomdark@arch.localdomain>
2025-07-06 19:58:33 +0300

    Initial commit

---
+++ install_theme.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+D=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+if [[ `id -u` -ne 0 ]] ; then
+	echo "Please run with sudo $0"
+	exit 1
+fi 
+
+THEME_DIR="/boot/grub/themes"
+mkdir -p "$THEME_DIR"
+
+if ! cp "$D"/steinsgrub/theme.txt "$D"/steinsgrub/*.pf2 "$D"/steinsgrub/*.png "$THEME_DIR"/; then
+    echo "Failed to copy theme files" >&2
+    exit 1
+fi
+
+echo "Theme successfully installed to $THEME_DIR"
